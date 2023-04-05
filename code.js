b8adace5a791dc7b27766854a43f9d2026613b60
Ankitraj7764 <ankitgupta56518@gmail.com>
2023-04-06 01:16:34 +0530

    edit

--- code.js
+++ code.js
@@ -14,22 +14,18 @@ socketTimeoutMS: 45000 })
   .catch(err => {
     console.error(err);
     process.exit(1);
-  });
-
-// Define the route for the breadth-first search
+  })
 app.get('/bfs', async (req, res) => {
   const startingNodeId = req.query.starting_node_id;
 
-  // Create a queue and a list of visited nodes
+
   const queue = [startingNodeId];
   const visited = [];
 
-  // Process nodes in the queue until it's empty
   while (queue.length > 0) {
-    // Remove the first node from the queue
+  
     const currentNodeId = queue.shift();
 
-    // Add the node's children to the end of the queue (if they exist)
     const currentNode = await Node.findById(currentNodeId).populate('left_child right_child');
     if (currentNode.left_child) {
       queue.push(currentNode.left_child._id);
@@ -38,14 +34,13 @@ app.get('/bfs', async (req, res) => {
       queue.push(currentNode.right_child._id);
     }
 
-    // Process the node
-    visited.push(currentNode.value);
+        visited.push(currentNode.value);
   }
 
   res.json({ visited });
 });
 
-// Start the server
+
 app.listen(3000, () => {
   console.log('Server started on port 3000');
 });
